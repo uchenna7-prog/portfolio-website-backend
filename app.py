f9228e0c4bb02c38160aeb5854e7ea6a43eda064
uchendu uchenna Daniel <uchenduuchenna7@gmail.com>
2025-11-12 23:33:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
-CORS(app,resources={r"/*":{"origins":"https://uchendu-uchenna-portfolio.vercel.app"}},allow_headers=["Content-Type","Authorization"],methods=["GET","POST","OPTIONS"])
+CORS(app,resources={r"/*":{"origins":"https://uchendu-uchenna-portfolio.vercel.app/"}},allow_headers=["Content-Type","Authorization"],methods=["GET","POST","OPTIONS"])
 
 
 app.config["MAIL_SERVER"] = "smtp.gmail.com"
